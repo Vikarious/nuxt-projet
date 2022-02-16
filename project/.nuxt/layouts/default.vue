30ed8243672c3db88c1e9fee1b52050d254bc355
Vikarious <fialaire.arnaud@free.fr>
2022-02-16 19:21:35 +0100

    Update default.vue

--- project/.nuxt/layouts/default.vue
+++ project/.nuxt/layouts/default.vue
@@ -1,3 +1,27 @@
 <template>
+<div>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <NuxtLink class="nav-link" to="/">Home <span class="sr-only">(current)</span></NuxtLink>
+      </li>
+      <li class="nav-item">
+        <NuxtLink class="nav-link" to="/ReactPage">React</NuxtLink>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+  </div>
+  </nav>
   <Nuxt />
+</div>
 </template>
